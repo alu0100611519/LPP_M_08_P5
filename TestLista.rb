11955c45c64c004e5b1d655440c735c071e1b7b9
alu0100405200 <alu0100405200@ull.edu.es>
2014-10-29 00:45:09 +0000

    Fix de respuestas

--- TestLista.rb
+++ TestLista.rb
@@ -3,23 +3,23 @@ require './lib/Test_LPP.rb'
 
 arrayPreguntas = []
 
-p1 = Pregunta.new("1.-) Cual es la salida del siguiente codigo Ruby?\n\tclass Xyx\n\t  def pots\n\t    @nice\n\t  end\n\tend\n\nxyz = Xyz.new\np xyz.pots",["#<Xyz:0xa00208>","nil","0","Ninguna de las anteriores"],4,1)
+p1 = Pregunta.new("1.-) Cual es la salida del siguiente codigo Ruby?\n\tclass Xyx\n\t  def pots\n\t    @nice\n\t  end\n\tend\n\nxyz = Xyz.new\np xyz.pots",["#<Xyz:0xa00208>","nil","0","Ninguna de las anteriores"],4,2)
 
 arrayPreguntas << p1
 
-p2 = Pregunta.new("2.-) La siguiente definicion de un hash en Ruby es valida:\n\thash_raro = {\n\t  [1,2,3] => Oject.new(),\n\t  Hash.new => :toto\n}",["Cierto","Falso"],2,0)
+p2 = Pregunta.new("2.-) La siguiente definicion de un hash en Ruby es valida:\n\thash_raro = {\n\t  [1,2,3] => Oject.new(),\n\t  Hash.new => :toto\n}",["Cierto","Falso"],2,1)
 
 arrayPreguntas << p2
 
-p3 = Pregunta.new("3.-) Cual es la salida del siguiente codigo Ruby?\n\tclass Array\n\t  def say_hi\n\t    \"HEY!\"\n\t  end\n\tend\n\np [1, \"bob\"].say_hi",["1","bob","HEY!","Ninguna de las anteriores"],4,2)
+p3 = Pregunta.new("3.-) Cual es la salida del siguiente codigo Ruby?\n\tclass Array\n\t  def say_hi\n\t    \"HEY!\"\n\t  end\n\tend\n\np [1, \"bob\"].say_hi",["1","bob","HEY!","Ninguna de las anteriores"],4,3)
 
 arrayPreguntas << p3
 
-p4 = Pregunta.new("4.-) Cual es el tipo del objeto en el siguiente codigo Ruby?\n\tclass Objeto\n\tend",["Una instancia de la clase Class","Una constante","Un objeto","Ninguna de las anteriores"],4,0)
+p4 = Pregunta.new("4.-) Cual es el tipo del objeto en el siguiente codigo Ruby?\n\tclass Objeto\n\tend",["Una instancia de la clase Class","Una constante","Un objeto","Ninguna de las anteriores"],4,1)
 
 arrayPreguntas << p4
 
-p5 = Pregunta.new("5.-) Es apropiado que una clase Tablero herede de una clase Juego",["Cierto","Falso"],2,1)
+p5 = Pregunta.new("5.-) Es apropiado que una clase Tablero herede de una clase Juego",["Cierto","Falso"],2,2)
 
 arrayPreguntas << p5
 
