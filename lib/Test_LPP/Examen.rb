681d2ff102adb9ec5ac1e2fd4c520aba11ab63e8
Javier Luis Moreno Villena <alu0100405200@ull.edu.es>
2014-11-19 10:28:32 +0000

    Pruebas de Exam terminadas

--- lib/Test_LPP/Examen.rb
+++ lib/Test_LPP/Examen.rb
@@ -15,7 +15,15 @@ class Exam
 		@apellido = gets.chomp
 		@wrong = 0
 		@right = 0
-		@eleccion
+		@Lis = lis
+	end
+	
+	def initialize (lis, nombre, apellidos)
+		raise ArgumentError, 'No es una lista' unless lis.is_a? Lista
+		@nombre = nombre
+		@apellido = apellidos
+		@wrong = 0
+		@right = 0
 		@Lis = lis
 	end
 
@@ -35,6 +43,20 @@ class Exam
 		@wrong
 	end
 
+	def testcheck (ary)
+		tmp = @Lis
+		i = 0
+		tmp.each do |q|
+			if q.responder(ary[i])
+				@right += 1
+			else
+				@wrong += 1
+			end
+			i += 1
+		end
+		@right	
+	end
+
 	def run
 		puts "/n/nExamen de LPP "
 		puts "Apellidos: #{@apellido}"
@@ -46,11 +68,10 @@ class Exam
 		tmp.each do |q|
 			puts "#{q.to_s}\n"
 			STDOUT.flush
-			@eleccion = gets.chomp
+			eleccion = gets.chomp
+			puts "\tRespuesta__ #{eleccion}\n"
 			
-			puts "\tRespuesta__ #{@eleccion}\n"
-			
-			if q.responder(@eleccion)
+			if q.responder(eleccion)
 				@right += 1
 			else
 				@wrong += 1
@@ -64,28 +85,7 @@ class Exam
 			toReturn << "SUSPENDIDO"
 		end
 		toReturn << " con una tasa de aciertos de #{@right} sobre #{@Lis.count}"
-		
 		puts toReturn
-		
 		true
-		
 	end 
-
-
-
 end
-
-
-        p1 = Pregunta.new("1.-) Cual es la salida del siguiente codigo Ruby?\n\tclass Xyx\n\t def pots\n\t @nice\n\t end\n\tend\n\nxyz = Xyz.new\np xyz.pots",["#<Xyz:0xa00208>","nil","0","Ninguna de las anteriores"],4,'2',2)
-        p2 = TrueFalse.new("2.-) La siguiente definicion de un hash en Ruby es valida:\n\thash_raro = {\n\t [1,2,3] => Oject.new(),\n\t Hash.new => :toto\n}",true)
-        p3 = Pregunta.new("3.-) Cual es la salida del siguiente codigo Ruby?\n\tclass Array\n\t def say_hi\n\t \"HEY!\"\n\t end\n\tend\n\np [1, \"bob\"].say_hi",["1","bob","HEY!","Ninguna de las anteriores"],4,'3',3)
-        p4 = Pregunta.new("4.-) Cual es el tipo del objeto en el siguiente codigo Ruby?\n\tclass Objeto\n\tend",["Una instancia de la clase Class","Una constante","Un objeto","Ninguna de las anteriores"],4,'1',2)
-        p5 = TrueFalse.new("5.-) Es apropiado que una clase Tablero herede de una clase Juego",false)
-        ary = [p1,p2,p3,p4,p5]
-        list = Lista.from_array(ary)
-        
-        #binding.pry
-
-	examen = Exam.new(list)
-	
-	examen.run
--- spec/Test_LPP_spec.rb
+++ spec/Test_LPP_spec.rb
@@ -1,7 +1,7 @@
 require './lib/Test_LPP/ListaEnlazada.rb'
 require './lib/Test_LPP/Preguntas.rb'
 require './lib/Test_LPP/version.rb'
-#require './lib/Test_LPP/Examen.rb'
+require './lib/Test_LPP/Examen.rb'
 require 'spec_helper.rb'
 
 describe Pregunta do
@@ -60,6 +60,31 @@ describe Node do
     end
 end
 
+describe Exam do
+    before do
+        @p1 = Pregunta.new("1.-) Cual es la salida del siguiente codigo Ruby?\n\tclass Xyx\n\t def pots\n\t @nice\n\t end\n\tend\n\nxyz = Xyz.new\np xyz.pots",["#<Xyz:0xa00208>","nil","0","Ninguna de las anteriores"],4,'2',2)
+        @p2 = TrueFalse.new("2.-) La siguiente definicion de un hash en Ruby es valida:\n\thash_raro = {\n\t [1,2,3] => Oject.new(),\n\t Hash.new => :toto\n}",true)
+        @p3 = Pregunta.new("3.-) Cual es la salida del siguiente codigo Ruby?\n\tclass Array\n\t def say_hi\n\t \"HEY!\"\n\t end\n\tend\n\np [1, \"bob\"].say_hi",["1","bob","HEY!","Ninguna de las anteriores"],4,'3',3)
+        @p4 = Pregunta.new("4.-) Cual es el tipo del objeto en el siguiente codigo Ruby?\n\tclass Objeto\n\tend",["Una instancia de la clase Class","Una constante","Un objeto","Ninguna de las anteriores"],4,'1',2)
+        @p5 = TrueFalse.new("5.-) Es apropiado que una clase Tablero herede de una clase Juego",false)
+        @ary = [@p1,@p2,@p3,@p4,@p5]
+        @list = Lista.from_array(@ary)
+		@examen = Exam.new(@list,"Pepe","Perez")
+    end
+    
+    it "El array [2,1,1,2,3] devuelve 5" do
+        expect(@examen.testcheck(['2','1','1','2','3'])).to eq 5
+    end
+    
+    it "El array [2,2,2,2,2] devuelve 2" do
+        expect(@examen.testcheck(['2','2','2','2','2'])).to eq 2
+    end
+    
+    it "El array [0,0,0,0,0] devuelve 0" do
+        expect(@examen.testcheck(['0','0','0','0','0'])).to eq 0
+    end
+    
+end
 describe Lista do
     before do
         @p1 = Pregunta.new("1.-) Cual es la salida del siguiente codigo Ruby?\n\tclass Xyx\n\t  def pots\n\t    @nice\n\t  end\n\tend\n\nxyz = Xyz.new\np xyz.pots",["#<Xyz:0xa00208>","nil","0","Ninguna de las anteriores"],4,'2',2)
