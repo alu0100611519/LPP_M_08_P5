c402dc64b18b57c435487d70b4fdb098f2a302e9
alu0100611519 <alu0100611519@ull.edu.es>
2014-11-19 10:06:36 +0000

    Creado el coveralls.yml

--- spec/Test_LPP.rb
+++ spec/Test_LPP.rb
@@ -1,6 +1,4 @@
-require 'spec_helper'
-require 'Test_LPP'
-
-describe testLPP do
-  it 'requires aditional testing'
-end
+require '~/lib/Test_LPP/ListaEnlazada.rb' 
+require './lib/Test_LPP/Preguntas.rb'
+require './lib/Test_LPP/Examen.rb'
+require './lib/Test_LPP/version.rb'
